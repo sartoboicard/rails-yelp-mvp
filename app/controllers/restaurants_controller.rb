185d74a07abffbcce74dd73b5b939bac8b13db51
Sarin Boivinpicard <sarinbp@gmail.com>
2020-08-11 21:18:59 -0400

    controllers almost done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class RestaurantsController < ApplicationController
+	def index
+		@restaurants = Restaurant.all
+	end
+	def show
+    @restaurant = Restaurant.find(params[:id])
+	end
+	def new
+    @restaurant = Restaurant.new
+	end
+	def create
+    @restaurant = Restaurant.new(params[:restaurant])
+		@restaurant.save
+		redirect_to restaurant_path(@restaurant)
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up database"
+Restaurant.destroy_all
+puts "Database is clean"
+
+10.times do 
+    restaurant = Restaurant.create(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.street_address,
+        category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+        #rating: rand(1..5)
+    )
+    puts "restaurant #{restaurant.id} has been created"
+end
