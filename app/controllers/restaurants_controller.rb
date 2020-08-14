f4fad440efa5de9d70b2ed76d0dcb79a7bd15e62
Sarin Boivinpicard <sarinbp@gmail.com>
2020-08-13 20:57:28 -0400

    ex 2 almost done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,8 +9,14 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
 	end
 	def create
-    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant = Restaurant.new(restaurant_params)
 		@restaurant.save
 		redirect_to restaurant_path(@restaurant)
+	end
+	
+	private
+
+	def restaurant_params
+	  params.require(:restaurant).permit(:name, :address, :category)
 	end
 end
